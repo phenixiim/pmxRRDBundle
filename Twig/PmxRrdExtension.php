4c3f0c09e3dc018f33a9853287ae7f31786f6daf
luxemate <thunderbird@inbox.lv>
2012-11-20 10:27:22 +0200

    FIX: Twig extension filters

--- Twig/PmxRrdExtension.php
+++ Twig/PmxRrdExtension.php
@@ -4,35 +4,36 @@ namespace Pmx\Bundle\RrdBundle\Twig;
 
 class PmxRrdExtension extends \Twig_Extension
 {
+    protected $container;
 
-    protected $diContainer;
-
-    public function __construct($diContainer)
+    public function __construct($container)
     {
-        $this->diContainer = $diContainer;
+        $this->container = $container;
     }
 
     public function getFilters()
     {
         return array(
-            'getRrdImage' => new \Twig_Function_Method($this, 'fetchGraphImageForRrdByName'),
-            'getRrdImageUrl' => new \Twig_Function_Method($this, 'fetchGraphUrlForRrdByName'),
+            'rrd_image' => new \Twig_Filter_Method($this, 'getRrdImage'),
+            'rrd_image_url' => new \Twig_Filter_Method($this, 'getRrdImageUrl'),
         );
     }
 
     public function getRrdImage($databaseName, $path = null)
     {
         $url = $this->getRrdImageUrl($databaseName, $path);
-        return "<img src='$url' />";
+
+        return sprintf('<img src="%s" />', $url);
     }
 
     public function getRrdImageUrl($databaseName, $path = null)
     {
-        $pmxGraph = $this->diContainer->get('pmx_rrd.graph');
-        //todo;
-        return '';
-    }
+        if (null === $path) {
+            $path = $this->container->get('pmx_rrd.graph_location');
+        }
 
+        return sprintf('%s/%s.png', $path, $databaseName);
+    }
 
     public function getName()
     {
